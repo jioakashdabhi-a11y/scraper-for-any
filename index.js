d66c33496910e0fed7b855352264010c4eb4e032
jioakashdabhi-a11y <jioakashdabhi@gmail.com>
2025-12-10 11:33:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,37 @@ console.log("👉 Scraping:", url);
 
 const crawler = new PlaywrightCrawler({
     headless: true,
+
+    // block detection reduce
+    preNavigationHooks: [
+        async ({ page }) => {
+            await page.setExtraHTTPHeaders({
+                "accept-language": "en-GB,en;q=0.9",
+            });
+
+            // wait for dom build
+            await page.waitForTimeout(3000);
+        },
+    ],
+
     requestHandler: async ({ page, request }) => {
         console.log(`Checking ${request.url}`);
 
-        const title = await page.locator('#productTitle').textContent();
-        const price = await page.locator('.a-price .a-offscreen').first().textContent();
+        try {
+            // Amazon slow loading → increase wait
+            await page.waitForSelector('#productTitle', { timeout: 60000 });
 
-        console.log("--------- Result -----------");
-        console.log("Title:", title?.trim());
-        console.log("Price:", price?.trim());
-        console.log("----------------------------");
+            const title = await page.locator('#productTitle').textContent();
+            const price = await page.locator('.a-price .a-offscreen').first().textContent();
+
+            console.log("--------- Result -----------");
+            console.log("Title:", title?.trim());
+            console.log("Price:", price?.trim());
+            console.log("----------------------------");
+        } catch (err) {
+            console.log("❌ FAILED");
+            console.log(err.message);
+        }
     },
 });
 
