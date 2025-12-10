a66195fe879227bd5778a3b0a865e463505a6ee3
jioakashdabhi-a11y <jioakashdabhi@gmail.com>
2025-12-10 11:40:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,41 +2,55 @@ import cheerio from "cheerio";
 
 const asin = process.argv[2];
 if (!asin) {
-    console.log("❌ ASIN not provided");
-    process.exit(1);
+  console.log("❌ ASIN not provided");
+  process.exit(1);
 }
 
 const url = `https://www.amazon.in/dp/${asin}`;
-
 console.log("👉 Scraping:", url);
 
 try {
-    const response = await fetch(url, {
-        headers: {
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-            "Accept-Language": "en-IN,en;q=0.9",
-        },
-    });
+  const response = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp",
+      "Accept-Encoding": "gzip, deflate",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Cache-Control": "no-cache",
+      "Pragma": "no-cache",
+      "DNT": "1",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "none",
+      "Upgrade-Insecure-Requests": "1",
+    },
+  });
 
-    const html = await response.text();
-    const $ = cheerio.load(html);
+  const html = await response.text();
 
-    const title = $("#productTitle").text().trim();
-    const price = $(".a-price .a-offscreen").first().text().trim();
-    const img = $("#landingImage").attr("src");
+  // detect block
+  if (html.includes("To discuss automated access")) {
+    console.log("🔴 Amazon blocked request");
+    process.exit(1);
+  }
 
-    if (!title) {
-        console.log("❌ Blocked OR Failed");
-        process.exit(1);
-    }
+  const $ = cheerio.load(html);
 
-    console.log("--------- Result -----------");
-    console.log("Title:", title || "-");
-    console.log("Price:", price || "-");
-    console.log("Image:", img || "-");
-    console.log("----------------------------");
+  const title = $("#productTitle").text().trim();
+  const price = $(".a-price .a-offscreen").first().text().trim();
+  const image = $("#landingImage").attr("src");
 
+  if (!title) {
+    console.log("❌ Blocked OR Failed");
+    process.exit(1);
+  }
+
+  console.log("--------- Result -----------");
+  console.log("Title:", title || "-");
+  console.log("Price:", price || "-");
+  console.log("Image:", image || "-");
+  console.log("----------------------------");
 } catch (err) {
-    console.log("Error:", err.message);
+  console.log("Error:", err.message);
 }
