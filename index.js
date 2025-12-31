20f455777b36426aed7b156e1325152c0c79fb07
jioakashdabhi-a11y <jioakashdabhi@gmail.com>
2025-12-31 12:54:14 +0000

    index.js change

--- index.js
+++ index.js
@@ -6,25 +6,18 @@ async function main() {
     const url = process.env.INPUT_URL;
     const chatId = process.env.INPUT_CHATID;
     const messageId = process.env.INPUT_MESSAGE_ID;
-    const selectors = JSON.parse(process.env.INPUT_SELECTORS);
 
-    console.log({ id, url, chatId, messageId, selectors });
+    const selectorsRaw = process.env.INPUT_SELECTORS;
+    const selectors = selectorsRaw ? JSON.parse(selectorsRaw) : null;
 
-    // const inputAsin = process.argv[2];
-
-    // if (inputAsin) {
-    //     const res = await scrapeProduct(inputAsin);
-    //     await sendUpdate(res);
-    // }
-
-    // const pending = await getPending();
-
-    // for (const asin of pending) {
-    //     const data = await scrapeProduct(asin);
-    //     await sendUpdate(data);
-    // }
-
-    return true;
+    console.log({
+        id,
+        url,
+        chatId,
+        messageId,
+        selectors
+    });
 }
+
 
 main();
