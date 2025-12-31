5c506988a8111b9c55c87a80876405ff582a2f14
jioakashdabhi-a11y <jioakashdabhi@gmail.com>
2025-12-31 12:31:24 +0000

    change new

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import { scrapeProduct } from "./scraper/productScraper.js";
 import { sendUpdate, getPending } from "./utils/config.js";
 
 async function main() {
-    console.log(process.argv)
-    console.log(process.env)
+    const id = process.env.INPUT_ID;
+    const url = process.env.INPUT_URL;
+    const chatId = process.env.INPUT_CHATID;
+    const messageId = process.env.INPUT_MESSAGE_ID;
+    const selectors = JSON.parse(process.env.INPUT_SELECTORS);
+
+    console.log({ id, url, chatId, messageId, selectors });
+
     // const inputAsin = process.argv[2];
 
     // if (inputAsin) {
