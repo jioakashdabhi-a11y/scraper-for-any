08b6fd8007e6e7d13f28ca99edc4fefd8ee720fb
jioakashdabhi-a11y <jioakashdabhi@gmail.com>
2025-12-31 14:27:57 +0000

    new update

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ import { scrapeProduct } from "./scraper/productScraper.js";
 import { sendUpdate, getPending } from "./utils/config.js";
 
 async function main() {
+    console.log(process.argv);
+
     const id = process.argv[2];
     const url = process.argv[3];
     const chatId = process.argv[4];
     const messageId = process.argv[5];
+    const selectors = process.argv[6]
+        ? JSON.parse(process.argv[6])
+        : null;
 
-    const selectorsRaw = process.argv[6];
-    const selectors = selectorsRaw ? JSON.parse(selectorsRaw) : null;
+    console.log({ id, url, chatId, messageId, selectors });
 
-    console.log({
-        id,
-        url,
-        chatId,
-        messageId,
-        selectors
-    });
 }
 
 
