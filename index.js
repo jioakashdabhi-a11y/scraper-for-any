1b5d4de3371884e92e3585558ac88a73ca004864
jioakashdabhi-a11y <jioakashdabhi@gmail.com>
2025-12-31 14:21:23 +0000

    change

--- index.js
+++ index.js
@@ -2,22 +2,30 @@ import { scrapeProduct } from "./scraper/productScraper.js";
 import { sendUpdate, getPending } from "./utils/config.js";
 
 async function main() {
-    const id = process.env.INPUT_ID;
-    const url = process.env.INPUT_URL;
-    const chatId = process.env.INPUT_CHATID;
-    const messageId = process.env.INPUT_MESSAGE_ID;
+  console.log("ENV CHECK:", {
+    id: process.env.INPUT_ID,
+    url: process.env.INPUT_URL,
+    chatId: process.env.INPUT_CHATID,
+    messageId: process.env.INPUT_MESSAGE_ID,
+    selectors: process.env.INPUT_SELECTORS,
+  });
 
-    const selectorsRaw = process.env.INPUT_SELECTORS;
-    const selectors = selectorsRaw ? JSON.parse(selectorsRaw) : null;
+  const id = Number(process.env.INPUT_ID);
+  const url = process.env.INPUT_URL;
+  const chatId = process.env.INPUT_CHATID;
+  const messageId = process.env.INPUT_MESSAGE_ID;
 
-    console.log({
-        id,
-        url,
-        chatId,
-        messageId,
-        selectors
-    });
+  const selectors = process.env.INPUT_SELECTORS
+    ? JSON.parse(process.env.INPUT_SELECTORS)
+    : null;
+
+  console.log({
+    id,
+    url,
+    chatId,
+    messageId,
+    selectors,
+  });
 }
-
 
 main();
